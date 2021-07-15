f77fdebd82ef7e39a00fa321210ef11434b7e850
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-15 08:58:33 +0100

    Added mongo and config file

--- router/app.py
+++ router/app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import yaml
+
+config_yaml = yaml.safe_load(open("../config/config.yaml"))
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://{}:{}@datafy.odhvq.mongodb.net/myFirstDatabase?retryWrites" \
+                          "=true&w=majority".format(config_yaml.get("mongo_user"), config_yaml.get("mongo_password"))
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def hello():
-    return "<p>Heheh</p>"
+    return "<p>Connected</p>"
+
+
+app.run(debug=True)
